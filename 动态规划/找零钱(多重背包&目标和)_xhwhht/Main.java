18d6a568456a345a6eb148418d312f7c9ed6714d
Macrad <2244695967@qq.com>
2021-09-09 17:48:40 +0800

    增加题目_Hw

--- 动态规划/找零钱(多重背包&目标和)_xhwhht/Main.java
+++ 动态规划/找零钱(多重背包&目标和)_xhwhht/Main.java
@@ -1,21 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-
         int amount = 30;
         int[] coins = new int[]{1,3,7,11,13,15};
-        int[] nums = new int[]{1,2,3,4,5,2};
-
 
         int max = Integer.MAX_VALUE;
         int[] dp = new int[amount + 1];
-
 
         //初始化dp数组为最大值
         for (int j = 0; j < dp.length; j++) {
             dp[j] = max;
         }
-
         //当金额为0时需要的硬币数目为0
         dp[0] = 0;
 
@@ -23,21 +17,13 @@ public class Main {
             //遍历硬币个数
             for (int j = amount; j >=  coins[i]; j--) {
                 //倒序遍历：多重背包每个硬币选择次数有限
-                for (int k = 1; k <nums[i] + 1 ; k++) {
-                    //防止数组越界 && 当 dp[j-coins[i]]为初始最大值时，说明没有符合的组合
-                    if (((j - k * coins[i]) >= 0) && (dp[j - k * coins[i]] != max) ) {
-                        dp[j] = Math.min(dp[j], dp[j - k*coins[i]] +k);
-                    }
+
+                    if ((dp[j - coins[i]] != max) ) {
+                        dp[j] = Math.min(dp[j], dp[j - k*coins[i]] +1);
                 }
             }
-            //打印dp数组
-            for (int j = 0; j <amount+1 ; j++) {
-                System.out.print(dp[j] );
-                System.out.print(" ");
-
-            }
-            System.out.println();
         }
+    }
         //打印结果
         dp[amount]= (dp[amount] == max )? -1 : dp[amount];  //三目运算
         System.out.println(dp[amount]);
---
+++ 基本算法题目/孤立岛屿(并查集)_xhwhht/Main.java
@@ -0,0 +1,169 @@
+package baidu;
+
+import java.util.Scanner;
+
+/*
+ * 这个题目在0-1矩阵中寻找岛的数量时，是向8个方向检索。
+ * 不同之前只能水平或者垂直方向检索。
+ * */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+        int  m = cin.nextInt();
+        int  n = cin.nextInt();
+        int [][] a = new int[m][n];
+
+        String ss = cin.nextLine();
+
+        for (int i = 0; i <m ; i++) {
+            String s = cin.nextLine();
+            String[] str = s.split("");
+
+            for (int j = 0; j < n; j++) {
+                if(str[j].equals("F")){
+                    a[i][j] = 1;
+                }
+                else{
+                    a[i][j] = 0;
+                }
+
+            }
+
+        }
+
+        System.out.println(countIslands(a));
+    }
+
+    //主要子函数
+    public static int countIslands(int a[][]) {
+        int n = a.length;
+        int m = a[0].length;
+
+        DisjointUnionSets dus = new DisjointUnionSets(n * m);
+
+        /*
+         * 下面的循环检查其邻居，如果两者都是1，则进行处理。
+         */
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                // 如果单元格的值为0，则什么都不做
+                if (a[j][k] == 0)
+                    continue;
+                System.out.println("新的位置：");
+                // 8个方向检查
+                if (j + 1 < n && a[j + 1][k] == 1)
+                    dus.union(j * (m) + k, (j + 1) * (m) + k);
+                if (j - 1 >= 0 && a[j - 1][k] == 1)
+                    dus.union(j * (m) + k, (j - 1) * (m) + k);
+                if (k + 1 < m && a[j][k + 1] == 1)
+                    dus.union(j * (m) + k, (j) * (m) + k + 1);
+                if (k - 1 >= 0 && a[j][k - 1] == 1)
+                    dus.union(j * (m) + k, (j) * (m) + k - 1);
+                if (j + 1 < n && k + 1 < m && a[j + 1][k + 1] == 1)
+                    dus.union(j * (m) + k, (j + 1) * (m) + k + 1);
+                if (j + 1 < n && k - 1 >= 0 && a[j + 1][k - 1] == 1)
+                    dus.union(j * m + k, (j + 1) * (m) + k - 1);
+                if (j - 1 >= 0 && k + 1 < m && a[j - 1][k + 1] == 1)
+                    dus.union(j * m + k, (j - 1) * m + k + 1);
+                if (j - 1 >= 0 && k - 1 >= 0 && a[j - 1][k - 1] == 1)
+                    dus.union(j * m + k, (j - 1) * m + k - 1);
+
+            }
+        }
+
+        //计算孤立岛屿
+        int[] c = new int[n * m];
+        int numberOfIslands = 0;
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < m; k++) {
+                if (a[j][k] == 1) {
+
+                    int x = dus.find(j * m + k);  //找公共子节点
+
+                    if (c[x] == 0) {
+                        numberOfIslands++;
+                        c[x]++;
+                    }
+
+                    else
+                        c[x]++;
+                }
+            }
+        }
+        return numberOfIslands;
+    }
+
+
+    //内部类
+    static class DisjointUnionSets{
+
+        int[] parent;
+        int[] rank;
+
+
+        //初始化
+        public DisjointUnionSets(int n) {
+            rank = new int[n];
+            parent = new int[n];
+            for (int i = 0; i < n; i++)
+                parent[i] = i;
+
+        }
+
+        //找到根节点
+
+        int find(int x) {
+            if (parent[x] != x) {
+                return find(parent[x]);
+            }
+
+            return x;
+        }
+
+        //将公共集合里的节点关联至最终根节点  关联数少的往大的靠 当前父节点x
+        void union(int x, int y) {
+
+            //寻找这俩个节点的父节点 并合并到公共父节点
+            int xRoot = find(x);
+            int yRoot = find(y);
+
+            if (xRoot == yRoot) {         //找到自己
+                return;
+            }
+
+            if (rank[xRoot] < rank[yRoot]) {
+                parent[xRoot] = yRoot;       //关联节点关系
+            }else if (rank[yRoot] < rank[xRoot]) {
+                parent[yRoot] = xRoot;       //
+            }else
+            {
+                parent[yRoot] = xRoot;       //这俩个节点关联数量一样 需要合并压缩路径
+                rank[xRoot] = rank[xRoot] + 1;
+            }
+
+            show(parent,rank);
+        }
+
+       void show(int [] parent,int [] rank)
+       {
+           int n= parent.length;
+           for (int i = 0; i < n; i++) {
+               System.out.print(parent[i]);
+               System.out.print(" ");
+
+           }
+           System.out.println();
+           for (int i = 0; i <n ; i++) {
+               System.out.print(rank[i]);
+               System.out.print(" ");
+
+           }
+           System.out.println();
+       }
+    }
+
+}
+
