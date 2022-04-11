1ccd5c2a71360de5656ccd534d92d2627d5e0936
Macrad <2244695967@qq.com>
2022-04-12 00:18:52 +0800

    注释

--- 剑指offer/JZ03_Macrad/JZ03.cpp
+++ 剑指offer/JZ03_Macrad/JZ03.cpp
@@ -2,7 +2,6 @@
 #include<string>
 #include<vector>
 
-
 using namespace std;
 class JZ03Solution {
 public:
@@ -11,14 +10,14 @@ public:
 	int num_temp;
 		for (int i=0;i < numbers.size();i++)
 		{
-			if (i == numbers[i])
+			if (i == numbers[i])       //数字和下标一致就不需要交换因为已经对应
 				;
 			else
-				if (numbers[numbers[i]] == numbers[i])
+				if (numbers[numbers[i]] == numbers[i])  //数字和数字作为下标处的数字一致说明重复
 					return numbers[i];
 				else
 				{
-					num_temp = numbers[numbers[i]];
+					num_temp = numbers[numbers[i]];    //将数字放在对应的下标处，如9 应该在numbers[9]
 					numbers[numbers[i]] = numbers[i];
 					numbers[i] = num_temp;
 				}	
@@ -32,7 +31,6 @@ public:
 
 int main()
 {
-	using namespace std;
 	vector<int>number;
 	int num;
 	
