a99376d61b9619cfd534bb0785098159fc63bd42
Macrad <2244695967@qq.com>
2022-04-20 10:32:43 +0800

    updata

--- 剑指offer/JZ03_Macrad/JZ03.cpp
+++ 剑指offer/JZ03_Macrad/JZ03.cpp
@@ -7,8 +7,8 @@ class JZ03Solution {
 public:
 	//----------------核心代码------------------------------//
 	int duplicate(vector<int>& numbers) {
-	int num_temp;
-		for (int i=0;i < numbers.size();i++)
+		int num_temp;
+		for (int i = 0; i < numbers.size(); i++)
 		{
 			if (i == numbers[i])       //数字和下标一致就不需要交换因为已经对应
 				;
@@ -20,7 +20,7 @@ public:
 					num_temp = numbers[numbers[i]];    //将数字放在对应的下标处，如9 应该在numbers[9]
 					numbers[numbers[i]] = numbers[i];
 					numbers[i] = num_temp;
-				}	
+				}
 
 		}
 		return -1;
@@ -33,16 +33,15 @@ int main()
 {
 	vector<int>number;
 	int num;
-	
-	while (cin >> num&& cin.get() != '\n' )
+
+	while (cin >> num && cin.get() != '\n')
 	{
 		number.emplace_back(std::move(num));
 
 	}
 
-	JZ03Solution Test;	
-	int result=Test.duplicate(number);
+	JZ03Solution Test;
+	int result = Test.duplicate(number);
 	cout << result;
 
 }
-
