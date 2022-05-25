684ac82dbda25342acded5ad4687b5ce4f6ff4b6
Macrad <2244695967@qq.com>
2022-05-25 11:57:45 +0800

    updata

--- 剑指offer/JZ06（链表）_Macrad/JZ06.cpp
+++ 剑指offer/JZ06（链表）_Macrad/JZ06.cpp
@@ -17,37 +17,35 @@ public:
 	//----------------核心代码------------------------------//
 	vector<int> res;
 	vector<int> printListFromTailToHead(ListNode* head) {
-    
-    //-----------栈-----------------------------------//
+
+		//-----------栈-----------------------------------//
 		stack<int>ListNodeVal;
-		cout << &head<<endl;
-		cout << head;
-		while (head!= NULL)
+		while (head != NULL)
 		{
 			ListNodeVal.push(head->val);
-	        head = head->next;
+			head = head->next;
 		}
 
 		while (!ListNodeVal.empty())
 		{
-		   res.push_back(ListNodeVal.top());
-		   ListNodeVal.pop();
+			res.push_back(ListNodeVal.top());
+			ListNodeVal.pop();
 		}
 
-/* //-----------递归（系统栈）-----------------------------------//
-	if (head != NULL)
-	{
-		if (head->next != NULL)
-		{
-		
-			printListFromTailToHead(head->next);
-			
-		}
-		res.push_back(head->val);
-	}
-	*/
+		/* //-----------递归（系统栈）-----------------------------------//
+			if (head != NULL)
+			{
+				if (head->next != NULL)
+				{
 
-	return res;
+					printListFromTailToHead(head->next);
+
+				}
+				res.push_back(head->val);
+			}
+			*/
+
+		return res;
 	}
 
 };
@@ -55,80 +53,61 @@ public:
 
 int main()
 {
-	ListNode* head = new ListNode(0);
-	ListNode* cur = head;
+	ListNode* beforeHead = new ListNode(-1);
+	ListNode* cur, *head;
+	cur = beforeHead;
 
-	int n,val;
+	int n, val;
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> val;
-		ListNode* newNode = new ListNode(val);
+
+		ListNode* newNode = new ListNode(val);  //导致内存会有其他位置存此节点的地址
 		cur->next = newNode;
 		cur = newNode;
 	}
+	head = beforeHead->next;
+	delete beforeHead;
 
-	//ListNode curRead = *head;
-	ListNode* curRead = head;
-	
-	
-	cout <<"放头节点的地址"<< endl;
-	cout << &(curRead) << endl;
+	cout << endl;
+	cout << "放头指针的地址:" << endl;
 	cout << &(head) << endl;
 
-
-	cout <<endl;
-	cout << "头节点->next 的地址" << endl;
-	cout << &(curRead->next) << endl;
-	cout << &(head->next) << endl;
-
-
-	cout << "头节点的地址放的内容;val的地址" << endl;
+	cout << endl;
+	cout << "首节点的地址（==首节点value的地址）:" << endl;
 	cout << head << endl;
-	cout << *(&(curRead))<<endl;
-	cout << *(&(head))<<endl;
+	cout << &(head->val) << endl;
 
-	cout << endl;
-	cout << *(&(curRead->next)) << endl;
-	cout << *(&(head->next)) << endl;
-	cout << (curRead->next) << endl;
-	cout << (head->next) << endl;
-
-	cout << "头节点的Val地址" << endl;
-	cout << &((*(&(curRead)))->val) << endl;
-	cout << &((*(&(head)))->val) << endl;
-
-	cout << "头节点的Val" << endl;
-	cout << (*(&(curRead)))->val<< endl;
-	cout << (*(&(head)))->val << endl;
-
-	cout << "头节点的next即第一节点的地址（ListNode数据结构）" << endl;
-	cout << (*(&(curRead)))->next << endl;
-	cout << (*(&(head)))->next << endl;
 	
-	cout << "打印链表:" << endl;
-	while (curRead!=NULL) //curRead！=NULL 和后面不一样curRead->next!=NULL  curRead地址 curRead->next curRead地址里放的内容
-	{
-		//cout << &(curRead->val)<< ' '<< curRead<<' '<< *(&(curRead->val));
-		//cout << &(curRead->next);
-		cout << curRead->val;
-		curRead = curRead->next;
-		cout << endl;
-	}
 	cout << endl;
-
+	cout << "打印链表:" << endl;
+	
+	ListNode* curRead = head;
+	while (curRead != NULL) //curRead！=NULL 和后面不一样curRead->next!=NULL  curRead地址 curRead->next curRead地址里放的内容
+	{
+		cout << curRead->val << ' ';
+		curRead = curRead->next;
+	}
+	
+	
+	cout << endl;
+	cout << endl;
+	cout << "打印逆序输出的结果:" << endl;
 
 	JZ06Solution Test;
 	vector<int>res;
-    res=Test.printListFromTailToHead(head);
-	
-	cout << endl;
+	res = Test.printListFromTailToHead(head);
 	for (int num : res)
 	{
-		cout << num<<' ';
+		cout << num << ' ';
 	}
 	cout << endl;
+	cout << endl;
+	cout << "打印逆序输出后的头指针的地址以及指向首节点的地址:" << endl;
 	cout << &head << endl;
 	cout << head;
+	cout << endl;
+	
 }
 
