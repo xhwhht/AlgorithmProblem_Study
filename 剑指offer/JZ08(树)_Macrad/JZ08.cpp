ae21cc9ffeaf95c44f44a0066b931bbfd29e957d
Macrad <2244695967@qq.com>
2022-05-30 22:35:27 +0800

    updata

---
+++ 剑指offer/JZ08(树)_Macrad/JZ08.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+/*
+struct TreeLinkNode {
+	int val;
+	struct TreeLinkNode *left;
+	struct TreeLinkNode *right;
+	struct TreeLinkNode *next;
+	TreeLinkNode(int x) :val(x), left(NULL), right(NULL), next(NULL) {
+
+	}
+};
+*/
+class Solution {
+public:
+	TreeLinkNode* GetNext(TreeLinkNode* pNode) {
+		if (!pNode)return nullptr;
+		if (pNode->right)
+		{
+			pNode = pNode->right;
+			while (pNode->left)pNode = pNode->left;
+			return pNode;
+		}
+		while (pNode->next)
+		{
+			TreeLinkNode* root = pNode->next;
+			if (root->left == pNode)return root;
+			else pNode = root;
+		}
+		return nullptr;
+	}
+};
