d29ca6b6622b40da9ba5476eef5693765979a025
Macrad <2244695967@qq.com>
2022-06-06 21:45:02 +0800

    updata

---
+++ 剑指offer/JZ56-I(位运算)_Macrad/JZ56.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+
+
+class JZ65Solution {
+public:
+	//----------------核心代码------------------------------//
+	int add(int a, int b)
+	{
+		while (b != 0)
+		{
+			int c = (unsigned int)(a&b) << 1;
+			a = a ^ b;
+			b = c;
+
+		}
+		return a;
+	}
+};
+
+
+int main()
+{
+	JZ65Solution Test;
+	int a = -1,b=3,res;
+
+	res = Test.add(a, b);
+	cout << res;
+}
