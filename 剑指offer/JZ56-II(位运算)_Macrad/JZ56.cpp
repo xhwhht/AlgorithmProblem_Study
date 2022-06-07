a83d363b7290e396a46062c45d235ddf3ed15d1e
Macrad <2244695967@qq.com>
2022-06-07 22:43:30 +0800

    updata

---
+++ 剑指offer/JZ56-II(位运算)_Macrad/JZ56.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include<string>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+
+
+class JZ56Solution {
+public:
+	//----------------核心代码------------------------------//
+	int singleNumbers(vector<int>& nums) {
+	
+		int  count[32] = { 0 };
+		unsigned int res = 0, m = 1;
+		for (int num : nums)
+		{
+			m = 1;
+			for (int i = 0; i < 32; i++)
+			{
+				if (m&num) { ++count[i]; }
+
+				m = m << 1;
+			}
+		}
+		for (int i = 31; i >= 0; i--)
+		{
+			res = res << 1;
+			res = res | (count[i] % 3);
+
+		}
+
+		return (int)res;
+
+	}
+};
+
+
+int main()
+{
+	JZ56Solution Test;
+    int res;
+	vector<int>input{ 9,1,7,9,7,9,7 };
+	res = Test.singleNumbers(input);
+	cout << res;
+}
