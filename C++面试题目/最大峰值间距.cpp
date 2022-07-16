9c57e16831f1117ec4ec3f8c8af6b449ef793437
Macrad <2244695967@qq.com>
2022-07-16 15:50:02 +0800

    updata

---
+++ C++面试题目/最大峰值间距.cpp
@@ -0,0 +1,56 @@
+﻿/*
+题目：给定一组数返回最大峰值间距
+eg：1 1----> 2
+    1 2 3 3 3 2 1 ->5
+数组两端算峰值
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main () {
+	
+	vector<int>blocks{ 1,2,2,3,4,4,4,3,3,3,2};
+
+	int len = blocks.size(),res=1,upFlag=1,equal, max_res=0,equal_res=1;
+	if (1 == len)  return 1;
+	if (2 == len)  return 2;
+
+	for (int i = 1; i < len; i++)
+	{
+		if (1 == upFlag)   //upflag 序列处于非严格增长时刻
+		{
+			if (blocks[i] > blocks[i - 1])
+			{
+				res++;	
+				equal_res = 1;  //如果增长就清零目前 记录的“峰值相等数据”因为这个是局部峰值 不可作为第i个峰值的数据
+			}
+			else if(blocks[i] == blocks[i - 1])
+			{
+				equal = blocks[i];
+				res++;
+				equal_res++;
+			}
+			else  //赠变减的时候 比较第i-1个峰值的时刻
+			{
+				upFlag = 0;
+				max_res = max(res,max_res);
+				res = equal_res;               //保存第i个峰值的数据（如果值一样的峰值）
+			}
+		}
+		else  //当前处于非严格递减的序列
+		{
+			if (blocks[i] <= blocks[i - 1])
+				res++;
+			else
+			{
+				upFlag = 1;
+				res++;
+			}
+		}
+	    
+	}
+
+	res= max(res + 1, max_res); //res+1 为整个区间最后一段满足条件的区块
+	cout << res;
+}
---
+++ leetcode/LeetCode300最长递增子序列.cpp
@@ -0,0 +1,56 @@
+﻿/*
+
+法一：动态规划 初始dp数组1 从0~i个状态跳到i 
+法二：贪心+二分法  需两个数组  7 8 9 1 2 10    789->189(res 3)->1 2 9(res 3)->12910(res 4) 递增序列78910 
+
+ */
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+    
+    int n=nums.size(),res=0;
+    vector<int>dp(n,1);
+
+    for(int i=0;i<n;++i)
+    {
+        for(int j=0;j<i;++j)
+        {
+            if(nums[j]<nums[i])
+                 dp[i]=max(dp[i],dp[j]+1);  //动态规划 从0~i个状态跳到i
+        }
+    res=max(res,dp[i]);
+    }
+    
+    return res;
+    }
+};
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+    
+    int n=nums.size();
+    int res=0,mid,right;
+
+    vector<int>tail(n);
+    tail[0]=nums[0];
+    for(int i=0;i<n;++i)
+    {
+        int left=0; //二分查找的起点是0
+        right=res;
+        while(left<right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[i]>tail[mid])
+                left=mid+1;
+            else
+                right=mid;
+        }
+        tail[left]=nums[i]; //选择位置插入
+        if(right==res) res++; //tail数组的末尾 right是下标从0 right=res res需要++
+    }
+    
+    return res;
+    }
+};
