cf5a3d59f31710c17d0e643dd3ffba2e236e8e54
Macrad <2244695967@qq.com>
2022-07-24 14:58:15 +0800

    updata

---
+++ 处理题目的IO/构建坐标类.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include<vector>
+
+
+struct myPos
+{
+	int x;
+	int y;
+}pos;
+using namespace std;
+
+int main() {
+	
+	int N;
+	cin >> N;               //N 组坐标
+	vector<myPos> VecPos;   //坐标类的数组
+	
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> pos.x;
+		cin >> pos.y;
+		VecPos.emplace_back(pos);
+	}
+
+
+    //打印输出
+	for (int i = 0; i < N; ++i)
+	{
+		cout << VecPos[i].x << " " << VecPos[i].y << endl;
+	}
+
+
+
+return 0;
+}
+
